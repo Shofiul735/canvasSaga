2840eb6ee496842b9c01facc283df1e5c00d7cf7
Md. Shofiul Islam <ytshofiul@gmail.com>
2023-01-21 16:12:14 +0600

    enhancement #2: Line drawing implemented

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ console.log(canvas);
 
 //Reference: https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
 const context = canvas.getContext('2d');
+
+// Rect
+context.fillStyle = `rgba(255,0,20,0.3)`;
 context.fillRect(100,100,100,100);
-console.log(context);
+context.fillStyle = `rgba(0,0,20,0.3)`;
+context.fillRect(400,100,100,100);
+
+// Line
+context.beginPath();
+context.moveTo(100,150);
+context.lineTo(200,400);
+context.lineTo(300,150);
+context.strokeStyle = `blue`;
+context.stroke()
+
 
 console.log(`%c Canvas is ready`,`background:green; color:white;`);
